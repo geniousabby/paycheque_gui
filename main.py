8f279dbbc70409ab7bfb76cacb8cb0e38a2f8031
geniousabby <abbyspam1228@gmail.com>
2026-04-30 10:18:10 -0700

    Pseudo code
    Made pseudo code to outline what i want to code

---
+++ main.py
@@ -0,0 +1,36 @@
+"""
+Pay cheque calculator
+
+"""
+
+# Import tkinter for GUI making
+
+# Set up the root window
+
+# Create and place GUI widgets on a grid
+
+# Function for doing the calculation for percent of taxes
+
+# Function for doing the calculation for percent of savings
+
+# Hours worked
+# Label
+# Entry
+
+# Pay per hour
+# Label
+# Radio button
+
+# Taxes
+# Label
+# List box
+
+# Savings
+# Label
+# scale
+
+# Commute
+# Label
+# check button
+
+# Calculate button
